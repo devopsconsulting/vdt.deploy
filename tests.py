25ac306890d96543be958c44d6c9588ca9f3ce79
Martijn Jacobs <martijn@fourdigits.nl>
2012-06-12 19:22:48 +0200

    kokj

---
+++ tests.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from CloudStack.Client import Client
+from deploy import CloudStackDeployment
+
+class MockCloudStack(object):
+    
+def listVirtualMachines(self, params):
+    return [
+        {'displayname': 'guiserver',
+         'name': 'koe',
+         'id': 4045,
+         'state': 'Starting'},
+        {'displayname': 'rabbitmq',
+         'name': 'rabbit',
+         'id': 4047,
+         'state': 'Running'},
+        {'displayname': 'guiserver',
+         'name': 'koe',
+         'id': 4045,
+         'state': 'Fornicating'},
+    ]
+
+class DeployToolTest(TestCase):
+    def setUp(self):
+        self.deploy = CloudStackDeployment()
+        self.deploy.client = MockCloudStack()
+
+    def test_do_status(self):
+        pass
