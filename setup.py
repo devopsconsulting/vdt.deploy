cd10890a9c66f9dfe57956838b06f88d37d50322
Lars van de Kerkhof <lars@basic.koe.nu>
2012-07-04 14:29:50 +0200

    version increased

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ configured as an lvs load balancer.
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(name='avira.deploy',
       version=version,
