f6806e92f8428f876a5f72b46362e4cecdeebfbe
Martijn Jacobs <martijn@fourdigits.nl>
2012-07-04 12:16:36 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+avira.deploy
+============
+
+avira.deployment provides a command line tool for deploying VM's in Cloudstack
+with a puppet role. The puppet role defines the installation and configuration
+of the server. For example a server with the role *lvs* will be installed and
+configured as an lvs load balancer.
+"""
+
+from setuptools import setup
+from setuptools import find_packages
+
+version = '0.1'
+
+setup(name='avira.deploy',
+      version=version,
+      description="Avira Deployment Tool",
+      long_description=__doc__,
+      classifiers=[],
+      # Get strings from
+      #http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Martijn Jacobs',
+      author_email='martijn@fourdigits.nl',
+      url='https://github.dtc.avira.com/VDT/avira.deploy',
+      license='Avira VDT 2012',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      namespace_packages=['avira'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'distribute',
+        'watchdog',
+        'daemon',
+        # -*- Extra requirements: -*-
+      ],
+      entry_points={'console_scripts':
+          ['avira-deploy=avira.deploy.deploy:main',
+           'avira-puppetbot=avira.deploy.puppetbot:main]},
+      )
+
