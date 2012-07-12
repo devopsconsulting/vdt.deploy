dff11f5c77a32b13e7d430f3bdec70e3008f89dd
Martijn Jacobs <martijn@fourdigits.nl>
2012-07-12 16:02:24 +0200

    Changed order so it maches deploy.py, added portfw command, extended list command

--- avira/deploy/fabfile.py
+++ avira/deploy/fabfile.py
@@ -12,51 +12,67 @@ env.user = 'root'
 __all__ = ('status', 'deploy', 'destroy', 'start', 'stop', 'reboot', 'list',
            'request', 'release', 'kick', 'ssh')
 
+
 @wraps(CloudstackDeployment.do_status)
 def status(all=''):
     run('/usr/bin/avira-deploy status %(all)s' % locals())
+
 
 @wraps(CloudstackDeployment.do_deploy)
 def deploy(name, cloudinit_config='', **kwargs):
     kwargs_string = " ".join(["%s=%s" % (key.replace('puppet', ''), value) for key, value in kwargs.items()])
     run('/usr/bin/avira-deploy deploy %(name)s %(kwargs_string)s %(cloudinit_config)s' % locals())
 
+
 @wraps(CloudstackDeployment.do_destroy)
 def destroy(machine_id):
     run('/usr/bin/avira-deploy destroy %(machine_id)s' % locals())
+
 
 @wraps(CloudstackDeployment.do_start)
 def start(machine_id):
     run('/usr/bin/avira-deploy start %(machine_id)s' % locals())
 
+
 @wraps(CloudstackDeployment.do_stop)
 def stop(machine_id):
     run('/usr/bin/avira-deploy stop %(machine_id)s' % locals())
+
 
 @wraps(CloudstackDeployment.do_reboot)
 def reboot(machine_id):
     run('/usr/bin/avira-deploy reboot %(machine_id)s' % locals())
 
+
 @wraps(CloudstackDeployment.do_list)
 def list(type):
-    allowed_types = ['templates', 'serviceofferings', 'diskofferings', 'ip']
+    allowed_types = ['templates', 'serviceofferings', 'diskofferings', 'ip', 'portforwardings']
     if type in allowed_types:
         run('/usr/bin/avira-deploy list %(type)s' % locals())
     else:
         print "type should be one of %(allowed_types)s" % locals()
 
+
 @wraps(CloudstackDeployment.do_request)
 def request(ip):
     run('/usr/bin/avira-deploy request %(ip)s' % locals())
+
 
 @wraps(CloudstackDeployment.do_release)
 def release(ip):
     run('/usr/bin/avira-deploy release %(ip)s' % locals())
 
+
+@wraps(CloudstackDeployment.do_portfw)
+def portfw(machine_id, ip_id, public_port, private_port):
+    run('/usr/bin/avira-deploy portfw %(machine_id)s %(ip_id)s %(public_port)s %(private_port)s' % locals())
+
+
+@wraps(CloudstackDeployment.do_ssh)
+def ssh(machine_id):
+    run('/usr/bin/avira-deploy ssh %(machine_id)s' % locals())
+
+
 @wraps(CloudstackDeployment.do_kick)
 def kick(machine_id):
     run('/usr/bin/avira-deploy kick %(machine_id)s' % locals())
-
-@wraps(CloudstackDeployment.do_kick)
-def ssh(machine_id):
-    run('/usr/bin/avira-deploy ssh %(machine_id)s' % locals())
