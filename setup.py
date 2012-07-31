238510f7ab64c30b01667ee5de1e5028f4d9207b
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-07-31 12:08:15 +0200

    updated version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ configured as an lvs load balancer.
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.9'
+version = '0.1.10'
 
 setup(name='avira.deploy',
       version=version,
