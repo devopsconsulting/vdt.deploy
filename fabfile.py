960d7c67058388893c68ab0b10c41d3cc2a5d3ff
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-08-14 00:05:03 +0200

    cleanup

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,10 @@
-from avira.deploy.deploy import CloudstackDeployment
-from functools import wraps as wraps
 import subprocess
-from config import PUPPETMASTER_SSH_PORT
+import functools
 
 from fabric.api import *
+
+from avira.deploy.deploy import CloudstackDeployment
+from avira.deploy.config import PUPPETMASTER_SSH_PORT
 
 PUPPETMASTER_IPADDRESS = subprocess.check_output(['python', 'avira/deploy/deploy.py', 'list', 'ip']).split()[1]
 env.hosts = ["%(PUPPETMASTER_IPADDRESS)s:%(PUPPETMASTER_SSH_PORT)s" % locals()]
@@ -13,43 +14,43 @@ __all__ = ('status', 'deploy', 'destroy', 'start', 'stop', 'reboot', 'list',
            'request', 'release', 'kick', 'ssh', 'portfw', 'clean', 'mco')
 
 
-@wraps(CloudstackDeployment.do_status)
+@functools.wraps(CloudstackDeployment.do_status)
 def status(all=''):
     run('/usr/bin/avira-deploy status %(all)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_deploy)
+@functools.wraps(CloudstackDeployment.do_deploy)
 def deploy(name, cloudinit_config='', **kwargs):
     kwargs_string = " ".join(["%s=%s" % (key.replace('puppet', ''), value) for key, value in kwargs.items()])
     run('/usr/bin/avira-deploy deploy %(name)s %(kwargs_string)s %(cloudinit_config)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_destroy)
+@functools.wraps(CloudstackDeployment.do_destroy)
 def destroy(machine_id):
     run('/usr/bin/avira-deploy destroy %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_clean)
+@functools.wraps(CloudstackDeployment.do_clean)
 def clean():
     run('/usr/bin/avira-deploy clean')
 
 
-@wraps(CloudstackDeployment.do_start)
+@functools.wraps(CloudstackDeployment.do_start)
 def start(machine_id):
     run('/usr/bin/avira-deploy start %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_stop)
+@functools.wraps(CloudstackDeployment.do_stop)
 def stop(machine_id):
     run('/usr/bin/avira-deploy stop %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_reboot)
+@functools.wraps(CloudstackDeployment.do_reboot)
 def reboot(machine_id):
     run('/usr/bin/avira-deploy reboot %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_list)
+@functools.wraps(CloudstackDeployment.do_list)
 def list(type):
     allowed_types = ['templates', 'serviceofferings', 'diskofferings', 'ip', 'portforwardings']
     if type in allowed_types:
@@ -58,27 +59,27 @@ def list(type):
         print "type should be one of %(allowed_types)s" % locals()
 
 
-@wraps(CloudstackDeployment.do_request)
+@functools.wraps(CloudstackDeployment.do_request)
 def request(ip):
     run('/usr/bin/avira-deploy request %(ip)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_release)
+@functools.wraps(CloudstackDeployment.do_release)
 def release(ip):
     run('/usr/bin/avira-deploy release %(ip)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_portfw)
+@functools.wraps(CloudstackDeployment.do_portfw)
 def portfw(machine_id, ip_id, public_port, private_port):
     run('/usr/bin/avira-deploy portfw %(machine_id)s %(ip_id)s %(public_port)s %(private_port)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_ssh)
+@functools.wraps(CloudstackDeployment.do_ssh)
 def ssh(machine_id):
     run('/usr/bin/avira-deploy ssh %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_kick)
+@functools.wraps(CloudstackDeployment.do_kick)
 def kick(machine_id=None, puppetrole=None):
     if puppetrole:
         run('/usr/bin/avira-deploy kick role=%(role)s' % locals())
@@ -86,7 +87,7 @@ def kick(machine_id=None, puppetrole=None):
         run('/usr/bin/avira-deploy kick %(machine_id)s' % locals())
 
 
-@wraps(CloudstackDeployment.do_mco)
+@functools.wraps(CloudstackDeployment.do_mco)
 def mco(*args, **kwargs):
     bar = " ".join(args)
     foo = ['%s=%s' % (key, value) for (key, value) in kwargs.iteritems()]
