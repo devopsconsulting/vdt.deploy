7b173330858722531eaafc6cdfcace3dfdfa0db0
Martijn Jacobs <martijn@fourdigits.nl>
2012-08-14 17:54:05 +0200

    First working implementation using mox to patch the client library

--- avira/deploy/tests.py
+++ avira/deploy/tests.py
@@ -1,19 +1,27 @@
 from unittest import TestCase
-from cloudstack.client import Client
-from avira.deploy import tool
+import cloudstack
 import mox
+import avira.deploy
+import avira.deploy.tool
 
 
 class DeployToolTest(TestCase):
     def setUp(self):
         self.mox = mox.Mox()
-        from pdb import set_trace; set_trace()
-        self.deploy = tool.CloudstackDeployment()
-        self.deploy.client = MockCloudStack()
-    
+        mock_client = self.mox.CreateMock(cloudstack.client.Client)
+        self.mox.StubOutWithMock(avira.deploy.tool, "Client")
+        avira.deploy.tool.Client("apiurl",
+                                 "apikey",
+                                 "secret").AndReturn(mock_client)
+        avira.deploy.tool.APIURL = "apiurl"
+        avira.deploy.tool.APIKEY = "apikey"
+        avira.deploy.tool.SECRETKEY = "secret"
 
     def tearDown(self):
         self.mox.UnsetStubs()
 
     def test_do_status(self):
-        pass
+        # we should have no VM's add the moment
+        self.mox.ReplayAll()
+        self.client = avira.deploy.tool.CloudstackDeployment()
+        self.mox.VerifyAll()
