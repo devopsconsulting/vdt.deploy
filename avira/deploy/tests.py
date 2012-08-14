d7a52e99c8918deebf26f3107cba74d4ff5660e4
Martijn Jacobs <martijn@fourdigits.nl>
2012-08-14 15:42:57 +0200

    Renamed to more logical name.

--- avira/deploy/tests.py
+++ avira/deploy/tests.py
@@ -1,33 +1,19 @@
 from unittest import TestCase
 from cloudstack.client import Client
-from deploy import CloudStackDeployment
-
-
-class MockCloudStack(object):
-    pass
-
-
-def listVirtualMachines(self, params):
-    return [
-        {'displayname': 'guiserver',
-         'name': 'koe',
-         'id': 4045,
-         'state': 'Starting'},
-        {'displayname': 'rabbitmq',
-         'name': 'rabbit',
-         'id': 4047,
-         'state': 'Running'},
-        {'displayname': 'guiserver',
-         'name': 'koe',
-         'id': 4045,
-         'state': 'Fornicating'},
-    ]
+from avira.deploy import tool
+import mox
 
 
 class DeployToolTest(TestCase):
     def setUp(self):
-        self.deploy = CloudStackDeployment()
+        self.mox = mox.Mox()
+        from pdb import set_trace; set_trace()
+        self.deploy = tool.CloudstackDeployment()
         self.deploy.client = MockCloudStack()
+    
+
+    def tearDown(self):
+        self.mox.UnsetStubs()
 
     def test_do_status(self):
         pass
