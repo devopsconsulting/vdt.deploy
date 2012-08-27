d9a867b940f003ff3aa08d302ad53cb98abcd87f
Martijn Jacobs <martijn@fourdigits.nl>
2012-08-27 14:39:18 +0200

    Fixed the test so it runs with nose

--- avira/deploy/tests/test_config.py
+++ avira/deploy/tests/test_config.py
@@ -3,19 +3,20 @@ from unittest import TestCase
 
 
 class DeployConfigTest(TestCase):
+
     def setUp(self):
+        # when we import the module it should generate a configfile in
+        # our home directory
         # we set the home directory to /tmp for the test
         os.environ["HOME"] = "/tmp"
         import avira.deploy.config
-        avira.deploy.config
+        reload(avira.deploy.config)
 
     def tearDown(self):
         if os.path.exists("/tmp/.aviradeployment.cfg"):
             os.remove("/tmp/.aviradeployment.cfg")
 
     def test_generate_config(self):
-        # when we import the module it should generate a configfile in
-        # our home directory
         self.assertEqual(os.path.isfile("/tmp/.aviradeployment.cfg"), True)
         # import something from config, it should have read it from the
         # generated configfile
