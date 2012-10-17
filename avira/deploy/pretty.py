b3fea2342fc1a092336aca6b294ddbed27c498ac
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-17 16:29:36 +0200

    Adapted portfw printing

--- avira/deploy/pretty.py
+++ avira/deploy/pretty.py
@@ -29,10 +29,10 @@ def public_ipaddresses_print(ipaddresses):
 
 def networks_print(networks):
     for network in networks:
-        print "%(id)5s   %(name)15s" % network
+        print "%(id)40s   %(name)15s" % network
 
 PORTFWD_LINE = \
-"%(id)5s   %(ipaddress)15s   %(publicport)5s to %(privateport)5s  on machine %(virtualmachineid)5s-%(virtualmachinedisplayname)s"
+"%(id)5s   %(ipaddress)15s   %(publicport)5s to %(privateport)5s  on machine %(virtualmachineid)40s %(virtualmachinedisplayname)25s"
 
 
 def portforwardings_print(portforwardings):
