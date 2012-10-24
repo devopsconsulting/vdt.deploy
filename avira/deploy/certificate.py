922ee34d23236b88ecce22de65fda7bb7ed1c74b
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-24 16:23:48 +0200

    Create a certificate file when it does not exist

--- avira/deploy/certificate.py
+++ avira/deploy/certificate.py
@@ -1,8 +1,11 @@
 from config import cfg
-
+import os
 
 def add_pending_certificate(machine_id):
     machine_id = str(machine_id)
+    if not os.path.exists(cfg.CERT_REQ):
+        f = open(cfg.CERT_REQ, 'w') 
+        f.close()
     with open(cfg.CERT_REQ, 'r+') as pending_certificates:
         if machine_id not in pending_certificates.read():
             pending_certificates.write(machine_id)
