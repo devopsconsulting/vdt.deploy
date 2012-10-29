1537aab202ff8066642308e8a173de4c79e8c8b8
Marty Brands <marty.brands@avira.com>
2012-10-29 15:53:51 +0100

    Fixed cloud.cfg setup

--- template/do.sh
+++ template/do.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copy cloud.cfg to a location that is visible from within the VM
+mkdir tmp
+cp ../avira/deploy/cloudinit\ configs\cloud.cfg tmp
 vagrant up
 vagrant halt
 vagrant destroy -f
+rm -rf tmp
