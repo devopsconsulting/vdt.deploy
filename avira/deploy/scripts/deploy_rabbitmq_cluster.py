cb7962fec4ad9dd3f3f9cb85f5a183afc0d99a48
Martijn Jacobs <martijn@fourdigits.nl>
2012-10-30 17:17:18 +0100

    Also deploy and destroy the disknode

--- avira/deploy/scripts/deploy_rabbitmq_cluster.py
+++ avira/deploy/scripts/deploy_rabbitmq_cluster.py
@@ -1,6 +1,8 @@
 import subprocess
 from time import sleep
 
+print subprocess.check_output(["avira-deploy",  "deploy",  "rabbitmqdisknode",  "role=rabbitmqdisknode", "environment=oe_dev"])
+sleep(2)
 for i in range(1,25):
     print subprocess.check_output(["avira-deploy",  "deploy",  "rabbitmqramnode%s" % i,  "role=rabbitmqramnode", "environment=oe_dev"])
     sleep(1)
--- avira/deploy/scripts/destroy_rabbitmq_cluster.py
+++ avira/deploy/scripts/destroy_rabbitmq_cluster.py
@@ -7,6 +7,6 @@ output = subprocess.check_output(["avira-deploy",  "status"])
 for x in output.split("\n"):
     row = re.split("\s+", x)
     if len(row) > 1:
-        if "ramnode" in row[1]:
-           print subprocess.check_output(["avira-deploy",  "destroy", row[2])
+        if "ramnode" in row[1] or "disknode" in row[1]:
+           print subprocess.check_output(["avira-deploy",  "destroy", row[2]])
            sleep(1)
