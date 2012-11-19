743307bcb7ed9c29c2f4c6d596c1d65b6c96bc98
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-11-19 11:52:50 +0100

    Implement argparse.

--- avira/deploy/tool.py
+++ avira/deploy/tool.py
@@ -1,26 +1,26 @@
 #! /usr/bin/python
 import sys
 import os
+import argparse
 
 from avira.deploy.config import cfg, configfile, main_template
 from avira.deploy.utils import load_plugin_by_name, UnknowPlugin
 
-def main():
-    if len(sys.argv) == 3:
-        if sys.argv[1] == "init":
-            try:
-                provider = load_plugin_by_name(sys.argv[2])
-                with open(configfile, "w") as f:
-                    f.write(main_template)
-                    f.write(provider.template)
-                    f.close()
-                print "Please edit your config at %s and restat the puppetbot if needed" % configfile
-            except UnknowPlugin as e:
-                print e.message
-            exit(0)
+def main(command=False, gen_config=False):
+    if gen_config:
+        try:
+            provider_plugin = load_plugin_by_name(gen_config)
+            with open(configfile, "w") as f:
+                f.write(main_template)
+                f.write(provider_plugin.template)
+                f.close()
+            print "Please edit your config at %s and restat the puppetbot if needed" % configfile
+        except UnknowPlugin as e:
+            print e.message
+        exit(0)
 
     if not os.path.isfile(configfile):
-        print "Please run avira-deploy init <provider> first\n"
+        print "Please run avira-deploy --gen-config=<provider> first\n"
         sys.exit(0)
 
     if not cfg.PUPPETMASTER_VERIFIED == '1':
@@ -32,8 +32,8 @@ def main():
         try:
             provider = load_plugin_by_name(cfg.PROVIDER)
             deploy = provider.Provider()
-            if len(sys.argv) > 1:
-                line = " ".join(sys.argv[1:])
+            if command:
+                line = " ".join(command)
                 deploy.onecmd(line)
             else:
                 try:
@@ -45,4 +45,11 @@ def main():
             exit(1)
 
 if __name__ == '__main__':
-    main()
+    p = argparse.ArgumentParser(description="Deployment tool for deploying VM's with puppet")
+    p.add_argument("command", default=False, nargs='*', help="the command to run.")
+    p.add_argument("--gen-config", default=False,
+                   help="Generate a config file at ~/.aviradeployment.cfg for the specified provider.")
+
+    args = p.parse_args()
+
+    main(args.command, args.gen_config)
